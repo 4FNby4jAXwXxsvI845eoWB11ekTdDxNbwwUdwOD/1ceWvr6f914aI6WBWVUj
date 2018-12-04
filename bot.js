71b6f70ee717142721d0f26962fd1b7c68dc22a8
4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD <45238840+4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD@users.noreply.github.com>
2018-12-04 11:38:44 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+});
+
+client.on('ready', async() => {
+var server = "514822456998101002"; //
+var channel = "514822457706807312";//
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein**')
+    },305);
+ });
+
+ 
+client.login("NTE5NDI5MzQxNjYzMTk5MjMy.DufNQg.tYL7Lq7k0WtyT6Bh0Dov5WnVjfo");
